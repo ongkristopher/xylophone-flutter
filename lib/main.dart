c6b733f259d5fcf1ccc5353b643d88bed9dee611
Kristopher Ong <ongkristopher@gmail.com>
2021-02-14 15:28:08 +0800

    Optimized and redesign the app

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,42 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play("note$soundNumber.wav");
+  }
+
+  Expanded buildKey({int soundNumber, Color noteColor}){
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(noteColor),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play("note1.wav");
-              },
-              child: Text("Click Me!"),
-            ),
-          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(soundNumber: 1, noteColor: Colors.red),
+              buildKey(soundNumber: 2, noteColor: Colors.orange),
+              buildKey(soundNumber: 3, noteColor: Colors.yellow),
+              buildKey(soundNumber: 4, noteColor: Colors.green),
+              buildKey(soundNumber: 5, noteColor: Colors.teal),
+              buildKey(soundNumber: 6, noteColor: Colors.blue),
+              buildKey(soundNumber: 7, noteColor: Colors.purple),
+            ],
+          )
         ),
       ),
     );
