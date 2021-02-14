3785eec2a7f89529a29b28157cc80529e1e70bc4
Kristopher Ong <ongkristopher@gmail.com>
2021-02-14 12:51:54 +0800

    migrate app to latest android standard | Add audio player for our xylophone app

---
+++ android/app/src/main/kotlin/com/ongkristopher/xylophone_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ongkristopher.xylophone-flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -9,7 +9,15 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(child: Text(adjectives.last)),
+          child: Center(
+            child: TextButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play("note1.wav");
+              },
+              child: Text("Click Me!"),
+            ),
+          ),
         ),
       ),
     );
